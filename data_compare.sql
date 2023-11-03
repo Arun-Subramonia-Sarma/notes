e6ba68fe5042d1fb5ffde957364d11beb92eff6c
Arun-Subramonia-Sarma <117281072+Arun-Subramonia-Sarma@users.noreply.github.com>
2023-11-03 11:39:29 +0530

    Update data_compare.sql

--- data_compare.sql
+++ data_compare.sql
@@ -4,9 +4,32 @@ WITH tbl AS
             TABLE_NAME
     FROM information_schema.tables
     WHERE TABLE_NAME not like 'pg_%'
-        AND table_schema in ('keycloak','portal','public')
+        AND table_schema in ('keycloak','public')
         AND TABLE_NAME not like ('aws%')
-        AND TABLE_NAME NOT in ('lock_monitor','alembic_version','databasechangelog','databasechangeloglock','filter_values','seq_values','trailer_history_tmp','trailers_v2_tmp')
+        AND TABLE_NAME NOT in ('lock_monitor',
+									'alembic_version',
+									'databasechangelog',
+									'databasechangeloglock',
+									'filter_values',
+									'pg_buffercache',
+									'trailer_history_copy_failure',
+									'trailer_history_tmp',
+									'trailer_live_replication_failure',
+									'trailer_status_intermediate_log',
+									'trailer_status_intermediate_logs_failure',
+									'trailer_status_live_replication_failure',
+									'trailers_copy_failure',
+									'trailers_intermediate_logs',
+									'trailers_intermediate_logs_failure',
+									'trailer_status_live_replication_failure',
+									'trailers_v2_tmp',
+									'map_updates',
+									'site_map_regions',
+									'site_maps',
+									'site_map_info',
+									'user_site_preferences',
+									'foo'
+								)
         AND TABLE_NAME not like '%partition%'
         AND TABLE_NAME not like '%default'
 )
@@ -16,3 +39,4 @@ SELECT table_schema,
 FROM tbl
 where tbl.TABLE_NAME not like '%partition%' and tbl.TABLE_NAME not like '%default'
 ORDER BY table_schema,TABLE_NAME;
+
